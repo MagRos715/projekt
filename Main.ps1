ce6db0c9c5b867aaf26abb86bedc544dea63a584
HenrikData <henrik.karlsson2.student@ksgyf.se>
2025-05-28 11:02:56 +0200

    Ändrat till relativa sökvägar

--- Main.ps1
+++ Main.ps1
@@ -1,8 +1,8 @@
 # Skapar Digital-Safe och undermappar.
-Import-Module "C:\Users\Jesper Olsén\Desktop\Scripting\GIT\projekt\Create-DigitalSafe.psm1"
+Import-Module "$PSScriptRoot\Create-DigitalSafe.psm1"
 
-# Importerar Modulen som genererar ett random, krypterat lösenord, sparar i fil.
-Import-Module "C:\Users\Henrik\Skolan\Scripting\projekt\Generate-RandomPassword.psm1"
+# Importerar modulen som genererar ett random, krypterat lösenord.
+Import-Module "$PSScriptRoot\Generate-RandomPassword.psm1"
 
 # Dekrypterar krypterade lösenord som sparats i textfiler.
-Import-Module "C:\Users\Admin\Projekt Powershell\Read-EncryptedPassword.psm1"
+Import-Module "$PSScriptRoot\Read-EncryptedPassword.psm1"
