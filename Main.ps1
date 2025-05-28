595a099f6369027f9b49aa045c83dee553fd4c92
HenrikData <henrik.karlsson2.student@ksgyf.se>
2025-05-28 10:42:33 +0200

    hhh

--- Main.ps1
+++ Main.ps1
@@ -1,5 +1,5 @@
 # Importerar Modulen som genererar ett random, krypterat lösenord, sparar i fil.
-Import-Module "C:\Users\Admin\Projekt Powershell\Generate-RandomPassword.psm1"
+Import-Module "C:\Users\Henrik\Skolan\Scripting\projekt\Generate-RandomPassword.psm1"
 
 # Dekrypterar krypterade lösenord som sparats i textfiler.
-Import-Module "C:\Users\Admin\Projekt Powershell\Read-EncryptedPassword.psm1"
+Import-Module "C:\Users\Henrik\Skolan\Scripting\projekt\Read-EncryptedPassword.psm1"
